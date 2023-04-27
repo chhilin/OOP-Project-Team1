cfac83dcc8210c5f5eaabe149b50753b2c9c2408
SOKI.CHHUOY <SOKI.CHHUOY@PNC.LAN>
2023-04-27 13:53:54 +0700

    [Create Method]

--- src/Airport.ts
+++ src/Airport.ts
@@ -1,10 +1,12 @@
 import { Airline } from "./airline/Airline";
-import {Gate} from ".gate/Gate";
+import { Gate } from "./gate/Gate";
+import { Route } from "./route/Route";
 export class Airport{
     private name: string;
     private address?: string;
     public airline: Airline[] = [];
     public gates: Gate[] = [];
+    public routes: Route[] = []
     constructor(name: string, address?: string){
         this.name = name;
         this.address = address;
@@ -15,5 +17,8 @@ export class Airport{
 
     addGate(gate: Gate){
         return this.gates.push(gate)
+    }
+    addRoute(route: Route){
+        return this.routes.push(route)
     }
 }
